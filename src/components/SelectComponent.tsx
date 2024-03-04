5861630ed8ee2571f5f44a4fc5c8bddd829426d7
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2024-03-04 13:19:22 +0545

    tsFixed

--- src/components/SelectComponent.tsx
+++ src/components/SelectComponent.tsx
@@ -6,24 +6,21 @@ import {
   FormLabel,
   Image,
 } from "@chakra-ui/react";
-import { Select, chakraComponents } from "chakra-react-select";
+import { OptionProps, Select, chakraComponents } from "chakra-react-select";
 import { socialPages } from "../data";
 
 export default function SelectComponent() {
-  interface OptionProps {
-    data: {
-      label: string;
-      value: string;
-      image: string;
-    };
-    children: React.ReactNode;
-  }
+  type SocialOption = {
+    label: string;
+    value: string;
+    image: string;
+  };
   const customComponents = {
-    Option: ({ data, ...props }: OptionProps) => (
-      <chakraComponents.Option innerProps={{ ...props }} {...props}>
-        <Image src={data.image} boxSize="24px" mr="2" />
-        {/* Display the image */}
-        {props.children}
+    Option: ({ children, ...props }: OptionProps<SocialOption>) => (
+      <chakraComponents.Option {...props}>
+        <Image src={props.data.image} boxSize="24px" mr="2" />
+
+        {children}
       </chakraComponents.Option>
     ),
   };
